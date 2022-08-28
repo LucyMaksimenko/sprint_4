6fb5e79aa41bf9b31142725533b277706ec52744
Lucy Maksimenko <l.maksimenko@yandex.com>
2022-08-28 03:29:35 +0300

    changes

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
-
 @pytest.fixture
 def driver():
     driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
--- page/base_page.py
+++ page/base_page.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 class BasePage:
-
     def __init__(self, driver):
         self.driver = driver
 
